1af17f2d89e1cdb087c07162ef28833f2f48bb0d
palme1337 <chris.palmer.1337@icloud.com>
2016-06-18 11:30:15 -0700

    Fudging with middleware

--- env/dev/clj/shitty_bot/middleware.clj
+++ env/dev/clj/shitty_bot/middleware.clj
@@ -1,11 +1,27 @@
 (ns shitty-bot.middleware
-  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+  (:require [config.core :refer [env]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
             [ring.middleware.json :refer [wrap-json-body]]
             [prone.middleware :refer [wrap-exceptions]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
+
+(defonce very-insecure-settings {
+                                 :security {:anti-forgery false}
+                                 })
+
+
+(defn wrap-dev [handler]
+  (fn [req]
+    (if (env :dev)
+      (-> handler
+          (wrap-defaults (merge site-defaults very-insecure-settings))
+          wrap-exceptions
+          wrap-reload))
+    (handler req)))
+
+
 (defn wrap-middleware [handler]
   (-> handler
       wrap-json-body
-      wrap-exceptions
-      wrap-reload))
+      wrap-dev))
